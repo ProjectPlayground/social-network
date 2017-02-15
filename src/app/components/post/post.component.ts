67906597f97fb43eff21f2fa524d6cdfa9d382c6
Luis Martinez <luisjmartnz@gmail.com>
2017-02-15 05:46:34 +0000

    Post with image functionality

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,20 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/filter';
+
+import { SocialService } from '../../services/social.service';
 
 @Component({
-    templateUrl: './post.component.html'
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.css']
 })
 
 export class PostComponent {
     createPostForm: FormGroup;
-    constructor( private fb: FormBuilder ) {
+    @ViewChild('imageInput') image: ElementRef;
+    
+    constructor( 
+        private fb: FormBuilder,
+        private socialService: SocialService ) {
             this.createPostForm = this.fb.group({
-               post: [''] 
+               post: ['']
             });
         }
         
     createPost() {
-        console.log(this.createPostForm.value);
-        this.createPostForm.reset();
+        let image = this.image.nativeElement.files;
+        let postData;
+        
+        this.socialService.sendPost(this.createPostForm.value)
+            .filter(obj =>  { 
+                if(image.length > 0){
+                    postData = obj;
+                    console.log(postData.key);
+                    return true;
+                }else{
+                    console.log('the post has been created');
+                }
+            })
+            .switchMap( postData => this.socialService.postImage(postData.key, image[0]))
+            .switchMap( imgRef => this.socialService.updatePost(postData.key, {image: imgRef.downloadURL}))
+            .subscribe(() => console.log('the post with image has been created'));
     }
+    
 }
--- src/app/services/social.service.ts
+++ src/app/services/social.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFire } from 'angularfire2';
+import * as firebase from 'firebase';
+import 'rxjs/add/operator/switchMap';
 
 import { UserService } from './user.service';
 import { AuthService } from './auth.service';
@@ -14,7 +16,27 @@ export class SocialService {
             
         }
         
-    createPost(postData) {
-        
+    sendPost(postData) {
+       return  this.auth.getAuth()
+            .switchMap(userInfo => this.createPost(postData, userInfo.auth.uid));
     }
+    
+    createPost(postData, uid) {
+        let posts = this.af.database.list('/posts');
+        postData.from = uid;
+        postData.timestamp = firebase.database['ServerValue'].TIMESTAMP;
+        return posts.push(postData);
+    }
+    
+    postImage(postId, image) {
+        let filename = postId + '.jpg';
+        
+        return firebase.storage().ref(`/posts/${filename}`).put(image);
+    }
+    
+    updatePost(postId, obj) {
+        console.log(obj);
+        return this.af.database.object(`/posts/${postId}`).update(obj);
+    }
+    
 }
